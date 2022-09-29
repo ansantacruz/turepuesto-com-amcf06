08e866f207c1e82c85e12a33169e0c4181882f59
Andres camilo Santacruz Borda <31628943+ansantacruz@users.noreply.github.com>
2022-09-29 16:37:19 -0500

    TR-000: I have created the initial styles configuration

--- src/app/pages/compras-tab/compras-tab.component.ts
+++ src/app/pages/compras-tab/compras-tab.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthServiceService } from '../../../services/auth-service.service';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-compras-tab',
@@ -11,12 +12,20 @@ export class ComprasTabComponent implements OnInit {
 
   constructor(
     private readonly authServiceService: AuthServiceService,
-    private readonly router: Router
+    private readonly router: Router,
+    private loadingCtrl: LoadingController
   ) { }
 
-  ngOnInit() {}
+  async ngOnInit() {
+
+  }
 
   async pasarComprador(): Promise<void> {
+    const loading = await this.loadingCtrl.create({
+      message: 'Espere un momento ',
+      duration: 1500,
+    });
+    loading.present();
     await this.authServiceService.login('COMPRADOR');
     this.router.navigate(['tabs/ofertas']);
   }
--- src/app/pages/ofertas-tab/ofertas-tab.component.ts
+++ src/app/pages/ofertas-tab/ofertas-tab.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { AuthServiceService } from '../../../services/auth-service.service';
 
 @Component({
@@ -11,12 +12,21 @@ export class OfertasTabComponent implements OnInit {
 
   constructor(
     private readonly authServiceService: AuthServiceService,
-    private readonly router: Router
+    private readonly router: Router,
+    private loadingCtrl: LoadingController
     ) { }
 
-  ngOnInit() {}
+  async ngOnInit() {
+
+
+  }
 
   async pasarVendedor(): Promise<void> {
+    const loading = await this.loadingCtrl.create({
+      message: 'Espere un momento ',
+      duration: 1500,
+    });
+    loading.present();
     await this.authServiceService.login('VENDEDOR');
     this.router.navigate(['tabs/compras']);
   }
